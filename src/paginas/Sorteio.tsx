5053c62443abb9b184c6d5b1168e7250bbdf1d72
Georgi Santana <georgi.andersson@hotmail.com>
2022-12-08 23:54:56 -0300

    implementado estilos no componente soteio

--- src/paginas/Sorteio.tsx
+++ src/paginas/Sorteio.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react"
+import Card from "../componentes/Card"
 import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes"
 import { useResultadoSorteio } from "../state/hook/useResultadoSorteio"
+import "./Sorteio.css"
 
 
 const Sorteio = () => {
@@ -18,23 +20,30 @@ const Sorteio = () => {
             setAmigoSecreto(resultado.get(participanteDaVez)!)
         }
     }
-    return(<section>
-        <form onSubmit={sortear}>
-            <select 
-                required 
-                name="participanteDavez" 
-                id="participanteDavez" 
-                placeholder="Selecione o seu nome"
-                value={participanteDaVez}
-                onChange={evento=> setParticipanteDaVez(evento.target.value)}
+    return (<Card>
+        <section className="sorteio">
+            <h2>Quem vai tirar o papelzinho?</h2>
+            <form onSubmit={sortear}>
+                <select
+                    required
+                    name="participanteDavez"
+                    id="participanteDavez"
+                    placeholder="Selecione o seu nome"
+                    value={participanteDaVez}
+                    onChange={evento => setParticipanteDaVez(evento.target.value)}
                 >
-                {participantes.map(participante => <option key={participante}>{participante}</option>)}
-            </select>
-            <button>Sortear</button>
-        </form>
-        {amigoScreto && <p role="alert">{amigoScreto}</p>}
-    </section>)
-
+                    {participantes.map(participante => <option key={participante}>{participante}</option>)}
+                </select>
+                <p>Clique em em sortear para ver quem é seu amigo secreto!</p>
+                <button className="botao-sortear">Sortear</button>
+            </form>
+            {amigoScreto && <p className="resultado" role="alert">{amigoScreto}</p>}
+            <footer className="sorteio">
+                <img src="/imagens/aviao.png" className="aviao" alt="Um desenho de um avião de papel" />
+            </footer>
+        </section>
+    </Card>)
 }
+
 
 export default Sorteio
